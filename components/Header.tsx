e93f1ae793b47bcb7efe27bfd6d106e91b6a2c70
W-OK-E <okhere21@gmail.com>
2025-08-04 20:46:22 +0530

    Adding Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,4 @@
+// components/Header.tsx
+export default function Header() {
+  return <header><h2>Gurukul Header</h2></header>;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import Header from '@/components/Header';
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <h1>Welcome to Gurukul!</h1>
+      <p>This is your new beginning.</p>
+    </main>
+  );
+}
