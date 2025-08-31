3233560bfa3354ab74551528f571d6716c3db7f6
W-OK-E <okhere21@gmail.com>
2025-08-31 11:20:10 +0530

    Changing authService Import

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,14 +1,14 @@
 // app/api/login/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
-import { authService } from '@/lib/auth'; // update path accordingly
+import { serverAuthService } from '@/lib/auth'; // update path accordingly
 import { serialize } from 'cookie';
 
 export async function POST(req: NextRequest) {
   const { email, password } = await req.json();
   console.log("EMail in the router:",email);
   console.log("Password in the router:",password);
-  const result = await authService.signIn({ email, password });
+  const result = await serverAuthService.signIn({ email, password });
   console.log("Checking if login was a success:",result.success);
   if (!result.success || !result.user) {
     return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
